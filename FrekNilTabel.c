f52be3ac5a853e69d6f1cc2017cded2d73191b85
gheraldabraham <gherald061204@gmail.com>
2024-06-10 19:26:35 +0700

    Create FrekNilTabel.c

---
+++ FrekNilTabel.c
@@ -0,0 +1,54 @@
+/* Nama         : Gherald Abraham
+   NIM          : 24060123140210
+   Kelas        : A
+   Nama Program : FrekNilTabel.c
+   Deskripsi    : Mengecek Frekuensi Tabel
+*/
+
+
+#include <stdio.h>
+
+#define MAX_SIZE 100
+
+int main(){
+
+    //Kamus
+    int t[MAX_SIZE];
+    int freq[MAX_SIZE] = {0};
+    int n, i, j, count = 0;
+
+    //Algoritma
+    scanf("%d", &n);
+    for ( i = 0; i < n; i++)
+    {
+        scanf("%d", &t[i]);
+    }
+    
+    for ( i = 0; i < n; i++)
+    {
+        freq[t[i]]++;
+    }
+    
+    printf("Angka yang muncul lebih dari sekali : ");
+    for ( i = 0; i < n; i++){
+        if (freq[t[i]] > 1)
+        {
+            count = 0;
+            for ( j = 0; j < i; j++)
+            {
+                if (t[i] == t[j])
+                {
+                    count = 1;
+                    break;
+                }
+                
+            }
+            
+            if (!count)
+            {
+                printf("%d", t[i]);
+            }
+        }
+    }
+    
+}
